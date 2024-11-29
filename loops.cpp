c863dc22ee1afb60809167c8406b521ad1487bb6
Audumbar More <98267939+audu0132@users.noreply.github.com>
2024-11-29 08:45:20 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int count=1;
+
+    while(count <= 6000000){
+        cout << count << "\n";
+        count++;
+    }
+
+    cout << endl;
+    return 0;
+}
---
+++ n-value.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 50;
+    int i = 1;
+
+    while (i<= n) {
+        cout << i << "\n";
+        i++;
+    } 
+
+    cout<< endl;
+    return 0;
+}
