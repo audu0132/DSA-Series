a15093d9624408db9752d2006ed4f44ed8295e05
Audumbar More <98267939+audu0132@users.noreply.github.com>
2024-12-01 10:24:37 +0530

    Update for loop.cpp
    updated code

--- for loop.cpp
+++ for loop.cpp
@@ -5,7 +5,7 @@ int main (){
     int n=100;
     for(int i=1; i<=n; i = i+5){
         cout << i << "\n";
-    }
-    cout << endl;
-    return 0;
-}
+        }
+        cout << endl;
+        return 0;
+        }
